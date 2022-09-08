cc8b08b14642d3a7b2d9ab5565c8e565346c499c
Seungmin Lee <96819922+Soong-moo@users.noreply.github.com>
2022-09-08 19:09:04 +0900

    feat : reviseProduct Method

---
+++ marketStudy/src/marketStudy/Product.java
@@ -0,0 +1,50 @@
+package marketStudy;
+
+//import java.util.HashMap;
+
+public class Product {
+
+	private String name;
+	private int price;
+
+	ProductList pro = new ProductList(); // ProductList의 a ArrayList 전역 변수 해제
+
+	public Product() {
+	};
+
+	public Product(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	/*
+	 * public static HashMap<String, Integer> test = new HashMap<String, Integer>();
+	 * 
+	 * public void setProduct(String name, int price) { test.put(name, price); }
+	 */
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	// 가격 음수 판별
+	public boolean checkPrice(int price) {
+		if (price < 0)
+			return false;
+		else
+			return true;
+	}
+
+}
---
+++ marketStudy/src/marketStudy/ProductManagement.java
@@ -0,0 +1,94 @@
+package marketStudy;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class ProductManagement {
+
+	ProductList pro = new ProductList();
+	Scanner sc = new Scanner(System.in);
+
+	// 상품 추가 (1 parameter test)
+	public void addProduct(String name) {
+		System.out.println(name);
+	}
+
+	// 상품 추가
+	public void addProduct(String name, int price) {
+		Iterator<Product> itr = pro.a.iterator();
+		while (itr.hasNext()) {
+			Product product = itr.next();
+			if (product.getName().equals(name)) {
+				System.out.println(name + "은(는) 이미 있는 상품입니다.");
+				return;
+			}
+			if (!product.checkPrice(price)) {
+				System.out.println("가격은 음수가 될 수 없습니다.");
+				return;
+			}
+		}
+
+		/*
+		 * if (price < 0) { System.out.println("가격은 음수가 될 수 없습니다."); return; }
+		 */
+		Product p = new Product(name, price);
+		pro.a.add(p);
+	}
+
+	// 상품 조회
+	public void showProduct() {
+		if (pro.a.size() == 0) {
+			System.out.println("상품 없음");
+			return;
+		}
+		Iterator<Product> itr = pro.a.iterator();
+		while (itr.hasNext()) {
+			Product product = itr.next();
+			System.out.printf("%s  %d\n", product.getName(), product.getPrice());
+		}
+	}
+
+	// 상품 제거
+	public void removeProduct(String name) {
+		Iterator<Product> itr = pro.a.iterator();
+		while (itr.hasNext()) {
+			Product product = itr.next();
+			if (product.getName().equals(name)) {
+				itr.remove();
+				return;
+			}
+		}
+		System.out.println("상품이 존재하지 않습니다.");
+	}
+
+	// 물건 수정
+	public void reviseProduct(String name) {
+		Iterator<Product> itr = pro.a.iterator();
+		while (itr.hasNext()) {
+			Product product = itr.next();
+			if (product.getName().equals(name)) {
+				System.out.print("상품명 재입력 : ");
+				String newName = sc.nextLine();
+				System.out.print("상품 가격 재입력 : ");
+				int newPrice = sc.nextInt();
+				product.setName(newName);
+				product.setPrice(newPrice);
+				return;
+			}
+		}
+		System.out.println("상품이 존재하지 않습니다.");
+	}
+	
+	
+	// 가격 알림
+	public void noticePrice(String name) {
+		Iterator<Product> itr = pro.a.iterator();
+		while (itr.hasNext()) {
+			Product product = itr.next();
+			if (product.getName().equals(name)) {
+				System.out.println(product.getName() + "의 가격은 " + product.getPrice() + "원 입니다.");
+				return;
+			}
+		}
+	}
+}
